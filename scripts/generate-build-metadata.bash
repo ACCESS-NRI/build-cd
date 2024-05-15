9274bd69bafdbfcab14ec7f7ca92adda9d8c3f00
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-05-15 11:52:31 +1000

    Upload Build Provenance Metadata (#63)
    * deploy-2-start.yml: Moved release into it's own job, added build db upload job
    
    * Added scripts/upload-build-metadata.bash
    
    * deploy-2-start.yml: Removed hash from spack.location.json
    
    * deploy-2-start.yml: Added fixes from org testing
    
    * deploy-2-start.yml: Added connection string secret
    
    * upload-build-metadata.bash: Updated json section names
    
    * upload-build-metadata.bash: modifications to the json structure based on ACCESS-NRI/build-cd@0f110c4038ba79c199c3508d07b538f1be0f64d8
    
    * deploy-2-start.yml: Split requirements.txt install into own step
    
    * deploy-2-start.yml: Reverted spack.location.json creation logic
    
    * Renamed upload-build-metadata.bash -> generate-build-metadata.bash
    
    * deploy-2-start.yml: Ported upload logic from generate-build-metadata.bash to inline script
    
    * deploy-2-start.yml: Update spack.location.json to be {hash: path} object
    
    * generate-build-metadata.bash: Added command getting the package hash from the lockfile, used to get the path
    
    * Added testing infrastructure
    
    * Added .gitignore
    
    * generate-build-metadata.bash: Moved release_url and created_at to model_build
    
    * generate-build-metadata.bash: Moved component builds into an array rather than multiple files
    
    * Updated test script and refactored outputs as a single file
    
    * deploy-2-start.yml: Removed looping since output is a single file

---
+++ scripts/generate-build-metadata.bash
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -x
+set -e
+
+### INPUTS ###
+# URL for the associated GitHub Release of $model_name.
+release_url=$1
+# Timestamp for the creation of the $release_url.
+release_time=$2
+# Path to the dir containing the spack.{lock,location.json}.
+json_dir=$3
+# directory that contains the <package>.json files
+output_dir=$4
+# Name of the model (or root-sbd) (eg. access-om2)
+model_name=$5
+
+# the rest of the model components for the given $model_name
+# (eg. for access-om2 there would be mom5, cice5, etc...)
+shift 5
+packages=( "$@" )
+
+### SCRIPT ###
+mkdir -p "$output_dir"
+
+spack=$(jq \
+  '{
+    version: .spack.version,
+    commit: .spack.commit
+  }' "$json_dir/spack.lock")
+
+model=$(jq \
+  --arg model "$model_name" \
+  --arg release_url "$release_url" \
+  --arg release_time "$release_time" \
+  --argjson spack "$spack" \
+  '.concrete_specs | to_entries[] | select(.value.name == $model)
+  | {
+      spack_hash: .key,
+      spec: (.value.name + "@" + .value.version),
+      created_at: $release_time,
+      release_url: $release_url,
+      spack_version: $spack
+  }' "$json_dir/spack.lock"
+)
+
+# construction of the initial build_metadata.json
+jq --null-input \
+  --argjson model "$model" \
+  '{
+    model_build: $model,
+    component_build: [],
+  }' > "$output_dir/build_metadata.json"
+
+for pkg in "${packages[@]}"; do
+  pkg_hash=$(jq --raw-output \
+    --arg pkg "$pkg" \
+    '.concrete_specs | to_entries[] | select(.value.name == $pkg) | .key' \
+    "$json_dir/spack.lock"
+  )
+
+  echo "Hash of $pkg is $pkg_hash"
+
+  install_path=$(jq --raw-output \
+    --arg pkg_hash "$pkg_hash" \
+    'to_entries[] | select(.key == $pkg_hash) | .value.path' \
+    "$json_dir/spack.location.json"
+  )
+
+  component=$(jq \
+    --arg pkg "$pkg" \
+    --arg install_path "$install_path" \
+    '.concrete_specs | to_entries[] | select(.value.name == $pkg)
+    | {
+      spack_hash: .key,
+      spec: (.value.name + "@" + .value.version),
+      install_path: $install_path
+    }' "$json_dir/spack.lock"
+  )
+
+  # piecewise construction of the entire build_metadata.json for each
+  # build_component
+  jq \
+    --argjson component "$component" \
+    '.component_build += [$component]' \
+    "$output_dir/build_metadata.json" > "$output_dir/build_metadata.json.tmp"
+
+  mv "$output_dir/build_metadata.json.tmp" "$output_dir/build_metadata.json"
+
+  cat "$output_dir/build_metadata.json"
+done
