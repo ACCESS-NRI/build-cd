44c7c593e8cd6bcd4852bcac51d938d0b5557770
Tommy Gatti <tommy.gatti@anu.edu.au>
2025-01-13 15:28:20 +1100

    Add testing infrastructure

---
+++ tests/scripts/jinja_template/outputs/release-body.md
@@ -0,0 +1,25 @@
+This is an official release of `access-om2` `2024.11.0`.
+
+## Deployment Information
+
+### `Gadi`
+
+Deployed using [spack 0.22](https://github.com/ACCESS-NRI/spack/tree/releases/0.22), [spack-packages 2024.09.20](https://github.com/ACCESS-NRI/spack-packages/releases/tag/2024.09.20) and [spack-config 2024.11.27](https://github.com/ACCESS-NRI/spack-config/releases/tag/2024.11.27).
+
+Deployed as module accessible using:
+
+```bash
+module use /g/data/vk83/prerelease/modules
+module load access-om2/2024.11.0
+```
+
+### `Setonix`
+
+Deployed using [spack 0.22](https://github.com/ACCESS-NRI/spack/tree/releases/0.22), [spack-packages 2024.09.20](https://github.com/ACCESS-NRI/spack-packages/releases/tag/2024.09.20) and [spack-config 2024.11.27](https://github.com/ACCESS-NRI/spack-config/releases/tag/2024.11.27).
+
+Deployed as module accessible using:
+
+```bash
+module use /some/dir/with/modules
+module load access-om2/2024.11.0
+```
---
+++ tests/scripts/jinja_template/test_render_deployment_info.py
@@ -0,0 +1,60 @@
+import pytest
+from pathlib import Path
+
+from scripts.jinja_template.render_deployment_info import build_deployment_context, _build_deployments_context_from_folder
+
+@pytest.fixture
+def deployments_folder(request):
+    return Path(request.path.parent) / "inputs"
+
+@pytest.fixture(
+    params=[
+        [
+            ("J2_MODEL", "model", "access-om2"),
+            ("J2_VERSION","version", "pr12-2"),
+            ("J2_ROOT_SBD","root_sbd","access-om2")
+        ],
+        [
+            ("J2_MODEL", "model", "access-om2")
+        ],
+    ]
+)
+def env_vars(request, monkeypatch):
+    for env_name, _, value in request.param:
+        monkeypatch.setenv(env_name, value)
+
+    yield request.param
+
+def test_build_deployment_context_envs(env_vars):
+    context = build_deployment_context(
+        template_prefix="J2_",
+        deployments_folder_path="tests/scripts/jinja_template/inputs",
+    )
+
+    for env_name, key, value in env_vars:
+        assert key in context, f"{key} (from {env_name}) does not exist in {context}"
+        assert value == context[f"{key}"], f"{value} does not equal expected {context[f"{key}"]}"
+
+
+def test_build_deployments_context_from_folder(deployments_folder):
+    context = _build_deployments_context_from_folder(deployments_folder)
+
+    assert len(context) == 2
+
+    assert context[0]["name"] == "Gadi"
+    assert context[0]["result"] == "success"
+    assert context[0]["release_version"] == "2024.11.16"
+    assert context[0]["spack_version"] == "0.22"
+    assert context[0]["spack_config_version"] == "2024.11.27"
+    assert context[0]["spack_packages_version"] == "2024.09.20"
+    assert context[0]["modules_location"] == "/g/data/vk83/prerelease/modules"
+    assert context[0]["spack_location"] == "/g/data/vk83/prerelease/apps/spack/0.22/spack"
+
+    assert context[1]["name"] == "Setonix"
+    assert context[1]["result"] == "success"
+    assert context[1]["release_version"] == "2024.11.16"
+    assert context[1]["spack_version"] == "0.22"
+    assert context[1]["spack_config_version"] == "2024.11.27"
+    assert context[1]["spack_packages_version"] == "2024.09.20"
+    assert context[1]["modules_location"] == "/some/dir/with/modules"
+    assert context[1]["spack_location"] == "/some/apps/spack/0.22/spack"
