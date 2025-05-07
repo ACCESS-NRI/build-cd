dba651494c68bd8603f620ec81da506370dc4df8
Tommy Gatti <tommy.gatti@anu.edu.au>
2025-05-07 16:06:33 +1000

    Release Provenance DB 2.0 (#269)
    * Remove old database infrastructure
    
    * Update .gitignore to exclude .vscode
    
    * Add scripts/release_provenance/tracking_services_data.py module
    
    * Add unit tests for tracking_services_data module
    
    * cd.yml: Now calls new tracking_services_data module
    
    Also removed Release metadata output, as this info is obtained in the module
    
    * validate-repo-version action: Output hash of the repo version, if valid
    
    * deploy-*.yml: Update metadata output and build-db-pkgs
    
    Now outputs model-name, model-deployment-repository, spack/config/packages git hash, root-spec-pkg hash
    
    * Update all access-nri/build-cd workflow_call refs to v5

---
+++ scripts/release_provenance/tracking_services_data.py
@@ -0,0 +1,407 @@
+import os
+import json
+import argparse
+import jsonschema
+import requests
+import re
+
+from pathlib import Path
+from typing import Any
+
+TRACKING_SERVICES_JSON_SCHEMA_VERSION = "1-0-0"
+TRACKING_SERVICES_JSON_SCHEMA_URL = f"https://raw.githubusercontent.com/ACCESS-NRI/schema/main/au.org.access-nri/tracking_services/release_provenance/telemetry/{TRACKING_SERVICES_JSON_SCHEMA_VERSION}.json"
+
+
+def format_deployment_information(
+    repository: str,
+    root_spec: str,
+    deployment_folder_path: Path,
+    metadata_folder_path: Path,
+) -> dict[str, Any]:
+    deployment_information: dict[str, Any] = {}
+
+    deployment_information.update(_format_tracking_services_header())
+
+    model_telemetry = _format_telemetry_of_model(repository, deployment_folder_path)
+    deployment_information["telemetry"] = model_telemetry
+
+    # We demarcate the deployment targets by the file extension (eg. *.Gadi, *.Setonix)
+    deployment_targets = {
+        file.split(".")[-1] for file in os.listdir(deployment_folder_path)
+    }
+
+    for deployment_target in deployment_targets:
+        components_telemetry = _format_telemetry_of_deployment_target(root_spec, deployment_target, deployment_folder_path, metadata_folder_path)
+        deployment_information["telemetry"]["deployment_targets"].append(components_telemetry)
+
+    print(deployment_information)
+
+    validate_deployment_information_against_schema(deployment_information)
+
+    return deployment_information
+
+
+def _format_tracking_services_header() -> dict[str, Any]:
+    """
+    Constructs and returns a dictionary representing the header data for tracking services.
+
+    Returns:
+        dict[str, Any]: A dictionary for static header data required by tracking services.
+    """
+    return {
+        "service": "release_provenance",  # Always "release_provenance" endpoint for this client
+        "version": TRACKING_SERVICES_JSON_SCHEMA_VERSION,  # May differ if the structure is updated
+        "telemetry": {},
+    }
+
+
+def _format_telemetry_of_model(
+    repository: str, deployments_folder_path: Path
+) -> dict[str, Any]:
+    # Telemetry of the model is the same for all deployment targets, so we only need to read the first one.
+    deployment_file = deployments_folder_path / os.listdir(deployments_folder_path)[0]
+
+    with open(deployment_file, "r") as outputs_file:
+        outputs = json.load(outputs_file)
+
+        version = outputs["release_deployment_version"]
+
+        # Get information from release
+        release_request_headers: dict[str, str] = {
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        if os.getenv("GITHUB_TOKEN") is not None:
+            release_request_headers["Authorization"] = (
+                f"Bearer {os.getenv('GITHUB_TOKEN')}"
+            )
+
+        release_json: dict[str, Any] = _get_release_data_of_model_or_raise(repository, release_request_headers, version)
+
+        return {
+            "model.name": outputs["model_name"],
+            "model.deployment_repository_url": outputs["model_deployment_repository"],
+            "model_deployment.version": version,
+            "model_deployment.status": "ACTIVE",  # A deployment defaults to ACTIVE status
+            "model_deployment.released_at": release_json["created_at"],
+            "model_deployment.release_url": release_json["html_url"],
+            "deployment_targets": [],
+        }
+
+def _get_release_data_of_model_or_raise(repository: str, headers: dict[str, str], version: str) -> dict[str, Any]:
+    release_request = requests.get(
+    f"https://api.github.com/repos/{repository}/releases/tags/{version}",
+    headers=headers,
+)
+
+    if release_request.status_code != 200:
+        raise ValueError(
+            f"Failed to fetch {version} release from {repository}: {release_request.status_code}"
+        )
+
+    return release_request.json()
+
+
+def _format_telemetry_of_deployment_target(
+    root_spec: str,
+    deployment_target: str,
+    deployment_folder_path: Path,
+    metadata_folder_path: Path,
+) -> dict[str, Any]:
+    with open(
+        deployment_folder_path / f"deploy-outputs.{deployment_target}", "r"
+    ) as outputs_file:
+        # Load relevant json files
+        outputs = json.load(outputs_file)
+
+        # TODO: Factor out _format_telemetry_of_model_components
+        return {
+            "deployment_target.name": deployment_target,
+            "deployment_target.spack_version": outputs["spack_version"],
+            "deployment_target.spack_git_hash": outputs["spack_git_hash"],
+            "deployment_target.spack_config_version": outputs["spack_config_version"],
+            "deployment_target.spack_config_git_hash": outputs["spack_config_git_hash"],
+            "deployment_target.spack_packages_version": outputs["spack_packages_version"],
+            "deployment_target.spack_packages_git_hash": outputs["spack_packages_git_hash"],
+            "deployment_target.module_use_location": outputs["deployment_modules_location"],
+
+            "spack_model.name": root_spec,
+            "spack_model.spack_package_hash": outputs["root_spec_pkg_hash"],
+            "spack_model.module_load_command": f"{root_spec}/{outputs['release_deployment_version']}",
+            "spack_model_components": _format_telemetry_of_model_components(
+                deployment_target, metadata_folder_path
+            ),
+        }
+
+
+def _format_telemetry_of_model_components(
+    deployment_target: str, metadata_folder_path: Path
+) -> list[dict[str, str]]:
+    model_components_telemetry: list[dict[str, str]] = []
+
+    with open(
+        metadata_folder_path / f"{deployment_target}.build-db-pkgs.json", "r"
+    ) as outputs_file:
+        # Load relevant json files
+        outputs: dict[str, Any] = json.load(outputs_file)
+
+        for component in outputs:
+            version = get_ref_from_spack_spec_version_or_raise(component["version"])
+
+            repo_url_at_version = get_repo_url_at_ref_or_raise(
+                component["url"], version
+            )
+
+            model_components_telemetry.append(
+                {
+                    "name": component["name"],
+                    "spack_package_hash": component["hash"],
+                    "version": version,
+                    "install_location": component["location"],
+                    "repository_url": repo_url_at_version,
+                }
+            )
+
+    return model_components_telemetry
+
+def get_ref_from_spack_spec_version_or_raise(spec_version: str) -> str:
+    """
+    Extracts the git ref from a Spack spec version string.
+
+    Args:
+        spec_version (str): The Spack spec version string.
+
+    Returns:
+        str: The extracted git ref.
+
+    Raises:
+        ValueError: If the spec version string is invalid.
+    """
+    groups_regex=r'^(?:git\.)?(?P<ref>[^=+~ ]+)'
+
+    match = re.search(groups_regex, spec_version)
+    if not match:
+        raise ValueError(f"Invalid spec version: {spec_version}")
+
+    return match.group("ref")
+
+
+def get_repo_url_at_ref_or_raise(repository_url: str, ref: str) -> str:
+    # Convert github URL to OWNER/REPO format
+    owner_repo_pattern = r"(?:github\.com)[:/]([^/]+/[^/.]+)(?:\.git)?$"
+    match = re.search(owner_repo_pattern, repository_url)
+    if not match:
+        raise ValueError(f"Invalid GitHub repository URL: {repository_url}")
+
+    repository = match.groups()[0]
+
+    # Set up request headers
+    repo_request_headers: dict[str, str] = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    if os.getenv("GITHUB_TOKEN") is not None:
+        repo_request_headers["Authorization"] = f"Bearer {os.getenv('GITHUB_TOKEN')}"
+
+    # Check if the version is a release, tag, or commit SHA
+    if url := _get_release_url_from_ref(repository, repo_request_headers, ref):
+        return url
+
+    if url := _get_tag_url_from_ref(repository, repo_request_headers, ref):
+        return url
+
+    if url := _get_sha_url_from_ref(repository, repo_request_headers, ref):
+        return url
+
+    raise ValueError(f"Failed to get tag or sha ref from {repository} at {ref}")
+
+def _get_release_url_from_ref(repository: str, headers: dict[str, str], version: str) -> str | None:
+    """
+    Fetches the release URL from the GitHub API for a given repository and version.
+
+    Args:
+        repository (str): The repository in OWNER/REPO format.
+        headers (dict[str, str]): The request headers for the GitHub API.
+        version (str): The version to fetch.
+
+    Returns:
+        str | None: The release URL if found, otherwise None.
+    """
+    release_request = requests.get(
+        f"https://api.github.com/repos/{repository}/releases/tags/{version}",
+        headers=headers,
+    )
+
+    if release_request.status_code == 200:
+        return f"https://github.com/{repository}/releases/tag/{version}"
+
+    return None
+
+def _get_tag_url_from_ref(repository: str, headers: dict[str, str], version: str) -> str | None:
+    """
+    Fetches the tag URL from the GitHub API for a given repository and version.
+
+    Args:
+        repository (str): The repository in OWNER/REPO format.
+        headers (dict[str, str]): The request headers for the GitHub API.
+        version (str): The version to fetch.
+
+    Returns:
+        str | None: The tag URL if found, otherwise None.
+    """
+    tag_request = requests.get(
+        f"https://api.github.com/repos/{repository}/git/refs/tags/{version}",
+        headers=headers,
+    )
+
+    if tag_request.status_code == 200:
+        return f"https://github.com/{repository}/releases/tag/{version}"
+
+    return None
+
+def _get_sha_url_from_ref(repository: str, headers: dict[str, str], version: str) -> str | None:
+    """
+    Fetches the commit URL from the GitHub API for a given repository and version.
+
+    Args:
+        repository (str): The repository in OWNER/REPO format.
+        headers (dict[str, str]): The request headers for the GitHub API.
+        version (str): The version to fetch.
+
+    Returns:
+        str | None: The commit URL if found, otherwise None.
+    """
+    sha_request = requests.get(
+        f"https://api.github.com/repos/{repository}/commits/{version}",
+        headers=headers,
+    )
+
+    if sha_request.status_code == 200:
+        return f"https://github.com/{repository}/commit/{version}"
+
+    return None
+
+def validate_deployment_information_against_schema(
+    deployment_information: dict[str, Any]
+) -> None:
+    """
+    Validates the deployment information against the JSON schema.
+
+    Args:
+        deployment_information (dict[str, Any]): The deployment information to validate.
+
+    Raises:
+        jsonschema.exceptions.ValidationError: If the deployment information does not conform to the schema.
+        jsonschema.exceptions.SchemaError: If the schema is invalid.
+        ValueError: If the schema cannot be fetched from the URL.
+    """
+    schema_request = requests.get(TRACKING_SERVICES_JSON_SCHEMA_URL)
+    if schema_request.status_code != 200:
+        raise ValueError(
+            f"Failed to fetch schema from {TRACKING_SERVICES_JSON_SCHEMA_URL}: {schema_request.status_code}"
+        )
+
+    try:
+        jsonschema.validate(
+            instance=deployment_information, schema=schema_request.json()
+        )
+    except jsonschema.ValidationError as e:
+        raise ValueError(f"Deployment information does not conform to the schema: {e}")
+    except jsonschema.SchemaError as e:
+        raise ValueError(f"Schema is invalid: {e}")
+
+
+def post_tracking_services_blob_or_raise(tracking_services_json_blob: dict[str, Any]):
+    if "TRACKING_SERVICES_POST_URL" not in os.environ or "TRACKING_SERVICES_POST_TOKEN" not in os.environ:
+        raise ValueError(
+            "Environment variables TRACKING_SERVICES_POST_URL and TRACKING_SERVICES_POST_TOKEN must be set to upload to Tracking Services."
+        )
+
+    # Get variables and secrets from environ
+    ts_url = os.environ["TRACKING_SERVICES_POST_URL"] + "/api/release-provenance/ingest_raw/"
+    ts_token = os.environ["TRACKING_SERVICES_POST_TOKEN"]
+
+    # Prepare the data to be sent to the Tracking Services API
+    telemetry_request_headers = {
+        "Content-type": "application/json",
+        "Authorization": "Token " + ts_token,
+    }
+
+    telemetry_response = requests.post(
+        ts_url,
+        headers=telemetry_request_headers,
+        data=json.dumps(tracking_services_json_blob),
+    )
+
+    if telemetry_response.status_code != 201:
+        raise ValueError(
+            f"Failed to post telemetry to Tracking Services: {telemetry_response.status_code}"
+        )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Script for generating and posting deployment information to tracking services release provenance database."
+    )
+
+    ## Args dealing with inputs
+    parser.add_argument(
+        "--repository",
+        type=str,
+        required=True,
+        help="Repository name of the model deployment repository",
+    )
+
+    parser.add_argument(
+        "--root-spec",
+        type=str,
+        required=True,
+        help="Name of the root spec of the model deployment",
+    )
+
+    parser.add_argument(
+        "--deployment-outputs",
+        type=str,
+        required=True,
+        help="Path to folder containing deploy-outputs.* files",
+    )
+    parser.add_argument(
+        "--metadata-outputs",
+        type=str,
+        required=True,
+        help="Path to folder containing metadata-outputs.* files",
+    )
+
+    ## Args dealing with optional outputs or uploads
+    parser.add_argument(
+        "--upload",
+        action="store_true",
+        help="Upload the tracking services JSON to the tracking services release provenance database",
+    )
+    parser.add_argument(
+        "--output", type=str, help="Path to output the tracking services JSON"
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    tracking_services_json_blob = format_deployment_information(
+        repository=args.repository,
+        root_spec=args.root_spec,
+        deployment_folder_path=Path(args.deployment_outputs),
+        metadata_folder_path=Path(args.metadata_outputs),
+    )
+
+    if args.output:
+        with open(args.output, "w") as output_file:
+            json.dump(tracking_services_json_blob, output_file, indent=2)
+
+    if args.upload:
+        post_tracking_services_blob_or_raise(tracking_services_json_blob)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/scripts/release_provenance/test_tracking_services_data.py
@@ -0,0 +1,217 @@
+import pytest
+
+from pathlib import Path
+from unittest.mock import patch, Mock
+
+from scripts.release_provenance.tracking_services_data import (
+    get_repo_url_at_ref_or_raise,
+    _format_tracking_services_header,
+    _format_telemetry_of_model,
+    _format_telemetry_of_deployment_target,
+    _format_telemetry_of_model_components,
+    get_ref_from_spack_spec_version_or_raise
+)
+from scripts.release_provenance.tracking_services_data import TRACKING_SERVICES_JSON_SCHEMA_VERSION
+
+class TestGetRepoUrlAtVersion:
+
+    @patch("scripts.release_provenance.tracking_services_data._get_release_url_from_ref")
+    def test__get_repo_url_at_ref_or_raise__valid_repo_and_version_release(self, release_url_mock):
+        # Test with a valid repository and version tag (that is a GitHub release)
+        repo_url = "https://github.com/access-nri/access-om2"
+        repo = "access-nri/access-om2"
+        version = "2024.03.0"
+
+        expected_release_url = f"https://github.com/{repo}/releases/tag/{version}"
+
+        # Mock the return value of _get_release_url_from_ref
+        release_url_mock.return_value = expected_release_url
+
+        actual_url = get_repo_url_at_ref_or_raise(repo_url, version)
+
+        assert actual_url == expected_release_url, f"Expected tag-based {expected_release_url}, but got {actual_url}"
+
+    @patch("scripts.release_provenance.tracking_services_data._get_tag_url_from_ref")
+    @patch("scripts.release_provenance.tracking_services_data._get_release_url_from_ref")
+    def test__get_repo_url_at_ref_or_raise__valid_repo_and_version_tag(self, release_url_mock, tag_url_mock):
+        # Test with a valid repository and version tag (that is a GitHub release)
+        repo_url = "https://github.com/access-nri/access-om2"
+        repo = "access-nri/access-om2"
+        version = "2024.03.0"
+
+        expected_tag_url = f"https://github.com/{repo}/releases/tag/{version}"
+
+        # Mock the return values
+        release_url_mock.return_value = None
+        tag_url_mock.return_value = expected_tag_url
+
+        actual_url = get_repo_url_at_ref_or_raise(repo_url, version)
+
+        assert actual_url == expected_tag_url, f"Expected tag-based {expected_tag_url}, but got {actual_url}"
+
+    @patch("scripts.release_provenance.tracking_services_data._get_sha_url_from_ref")
+    @patch("scripts.release_provenance.tracking_services_data._get_tag_url_from_ref")
+    @patch("scripts.release_provenance.tracking_services_data._get_release_url_from_ref")
+    def test_get_repo_url_at_ref_or_raise__valid_repo_and_version_sha(self, release_url_mock, tag_url_mock, sha_url_mock):
+        # Test with a valid repository and version SHA
+        repo_url = "https://github.com/access-nri/access-om2"
+        repo = "access-nri/access-om2"
+        version = "bf1f97ca75b942dd08506b88197cf0feaa1c694d"
+
+        expected_sha_url = f"https://github.com/{repo}/commit/{version}"
+
+        # Mock the return values
+        release_url_mock.return_value = None
+        tag_url_mock.return_value = None
+        sha_url_mock.return_value = expected_sha_url
+
+        # Call the function with the mocked return values
+        actual_url = get_repo_url_at_ref_or_raise(repo_url, version)
+
+        assert actual_url == expected_sha_url, f"Expected sha-based {expected_sha_url}, but got {actual_url}"
+
+    def test_get_repo_url_at_ref_or_raise__invalid_repo_url(self):
+        # Test with an invalid repository
+        repo_url = "https://github.invalid.com/access-nri/access-om2"
+        version = "2024.03.0"
+
+        with pytest.raises(ValueError):
+            get_repo_url_at_ref_or_raise(repo_url, version)
+
+    def test_get_repo_url_at_ref_or_raise__invalid_repo_structure(self):
+        # Test with an invalid repository
+        repo_url = "https://github.com/not-access-nri/not-access-om2"
+        version = "2024.03.0"
+
+        with pytest.raises(ValueError):
+            get_repo_url_at_ref_or_raise(repo_url, version)
+
+    @patch("scripts.release_provenance.tracking_services_data._get_sha_url_from_ref")
+    @patch("scripts.release_provenance.tracking_services_data._get_tag_url_from_ref")
+    @patch("scripts.release_provenance.tracking_services_data._get_release_url_from_ref")
+    def test_get_repo_url_at_ref_or_raise__invalid_version(self, release_url_mock, tag_url_mock, sha_url_mock):
+        # Test with an invalid version tag
+        repo_url = "https://github.com/access-nri/access-om2"
+        version = "invalid-tag"
+
+        # Mock the return values
+        release_url_mock.return_value = None
+        tag_url_mock.return_value = None
+        sha_url_mock.return_value = None
+
+        with pytest.raises(ValueError):
+            get_repo_url_at_ref_or_raise(repo_url, version)
+
+class TestFormatTrackingServicesHeader():
+    def test__format_tracking_services_header__valid(self):
+        # Test with a valid header
+        expected_header = {
+            "service": "release_provenance",
+            "version": f"{TRACKING_SERVICES_JSON_SCHEMA_VERSION}",
+            "telemetry": {}
+        }
+
+        actual_header = _format_tracking_services_header()
+
+        assert actual_header == expected_header, f"Expected {expected_header}, but got {actual_header}"
+
+class TestFormatTelemetryOfModel():
+
+    @patch("scripts.release_provenance.tracking_services_data._get_release_data_of_model_or_raise")
+    def test__format_telemetry_of_model_1_0_0__valid(self, release_data_mock):
+        expected_telemetry = {
+            "model.name": "ACCESS-OM3",
+            "model.deployment_repository_url": "https://github.com/ACCESS-NRI/ACCESS-OM3",
+            "model_deployment.version": "2025.01.2",
+            "model_deployment.status": "ACTIVE",
+            "model_deployment.released_at": "2025-03-27T05:16:15Z",
+            "model_deployment.release_url": "https://github.com/ACCESS-NRI/ACCESS-OM3/releases/tag/2025.01.2",
+            "deployment_targets": [],
+        }
+
+        # Mock the return value of _get_release_data_of_model_or_raise
+        release_data_mock.return_value = {
+            "created_at": "2025-03-27T05:16:15Z",
+            "html_url": "https://github.com/ACCESS-NRI/ACCESS-OM3/releases/tag/2025.01.2",
+        }
+
+        actual_telemetry = _format_telemetry_of_model("ACCESS-NRI/ACCESS-OM3", Path("tests/scripts/release_provenance/inputs/1-0-0/deploy-outputs.Gadi"))
+
+        assert actual_telemetry == expected_telemetry, f"Expected {expected_telemetry}, but got {actual_telemetry}"
+
+    @patch("scripts.release_provenance.tracking_services_data.requests.get", autospec=True)
+    def test__format_telemetry_of_model_1_0_0__invalid_repo(self, requests_mock):
+
+        # Mock the response of the requests.get call to simulate an invalid repository
+        requests_mock.return_value = Mock(status_code=404)
+
+        with pytest.raises(ValueError):
+            _format_telemetry_of_model("invalid-repo", Path("tests/scripts/release_provenance/inputs/1-0-0/deploy-outputs.Gadi"))
+
+    def test__format_telemetry_of_model_1_0_0__invalid_file(self):
+        with pytest.raises(FileNotFoundError):
+            _format_telemetry_of_model("ACCESS-NRI/ACCESS-OM3", Path("invalid-file-path"))
+
+class TestFormatTelemetryOfDeploymentTarget():
+    def test__format_telemetry_of_deployment_target_1_0_0__valid_with_no_components(self):
+        # Removing the components from the expected telemetry as that is tested in TestFormatTelemetryOfModelComponents
+        expected_telemetry_without_components = {
+            "deployment_target.name": "Gadi",
+            "deployment_target.spack_version": "0.22",
+            "deployment_target.spack_git_hash": "qwerty",
+            "deployment_target.spack_config_version": "2025.02.2",
+            "deployment_target.spack_config_git_hash": "asdfg",
+            "deployment_target.spack_packages_version": "2025.03.002",
+            "deployment_target.spack_packages_git_hash": "zxcvb",
+            "deployment_target.module_use_location": "/g/data/vk83/modules",
+
+            "spack_model.name": "access-om3",
+            "spack_model.spack_package_hash": "3jdoekn73de234jdh38dzojhdjfriksndpqj84903",
+            "spack_model.module_load_command": "access-om3/2025.01.2",
+        }
+
+        actual_telemetry = _format_telemetry_of_deployment_target("access-om3", "Gadi", Path("tests/scripts/release_provenance/inputs/1-0-0/deploy-outputs.Gadi"), Path("tests/scripts/release_provenance/inputs/1-0-0/deploy-metadata.Gadi"))
+        actual_telemetry_without_components = {k: v for k, v in actual_telemetry.items() if k not in ["spack_model_components"]}
+
+        assert actual_telemetry_without_components == expected_telemetry_without_components, f"Without components, expected {expected_telemetry_without_components}, but got {actual_telemetry_without_components}"
+
+class TestFormatTelemetryOfModelComponents():
+    def test__format_telemetry_of_model_components_1_0_0__valid(self):
+        expected_telemetry = [
+            {
+                "name": "access-test-component",
+                "spack_package_hash": "uyhr286gh2jyrh2uy3r2t6rg7236tr726t",
+                "version": "2025.04.000",
+                "install_location": "/g/data/vk83/releases/some/thing",
+                "repository_url": "https://github.com/access-nri/access-test-component/releases/tag/2025.04.000",
+            },
+        ]
+
+        actual_telemetry = _format_telemetry_of_model_components("Gadi", Path("tests/scripts/release_provenance/inputs/1-0-0/deploy-metadata.Gadi"))
+
+        assert actual_telemetry == expected_telemetry, f"Expected {expected_telemetry}, but got {actual_telemetry}"
+
+class TestGetRefFromSpackSpecVersionOrRaise():
+    def test_get_ref_from_spack_spec_version_or_raise__valid_no_git_no_version(self):
+        spack_spec_version = "2025.01.2"
+        expected_ref = "2025.01.2"
+
+        actual_ref = get_ref_from_spack_spec_version_or_raise(spack_spec_version)
+
+        assert actual_ref == expected_ref, f"Expected {expected_ref}, but got {actual_ref}"
+
+    def test_get_ref_from_spack_spec_version_or_raise__valid_with_git_no_version(self):
+        spack_spec_version = "git.2025.01.2"
+        expected_ref = "2025.01.2"
+
+        actual_ref = get_ref_from_spack_spec_version_or_raise(spack_spec_version)
+
+        assert actual_ref == expected_ref, f"Expected {expected_ref}, but got {actual_ref}"
+
+    def test_get_ref_from_spack_spec_version_or_raise__valid_with_git_with_version(self):
+        spack_spec_version = "git.2025.01.2=access-esm1.6"
+        expected_ref = "2025.01.2"
+
+        actual_ref = get_ref_from_spack_spec_version_or_raise(spack_spec_version)
+
+        assert actual_ref == expected_ref, f"Expected {expected_ref}, but got {actual_ref}"
