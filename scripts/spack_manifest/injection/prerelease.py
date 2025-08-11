13008b5787d939e413401a2dbf9dbaa7820aeb15
Tommy Gatti <tommy.gatti@anu.edu.au>
2025-08-12 08:18:15 +1000

    `v6` Changes Bundle (#306)
    * Injection of Modules over Checks (#295)
    
    * Add projection injection module
    
    * Add requirements for injection modules
    
    * Add tests for injection projection module, and inputs/requirements
    
    * Add prerelease injection module
    
    * Add tests for prerelease injection module
    
    * Run injection.projection and injection.prerelease modules
    
    * deploy-1-setup: Remove projection checks
    
    * Remove unneeded mergedeep requirement
    
    * Add module inits to folders
    
    * Add step for installing dependencies, update names for checkout steps
    
    * Used cd module prefix rather than custom working-directory so we can find the manifest
    
    * Added required input for prerelease modification, always print out updated manifest at end
    
    * Add back EOT
    
    * Update non-root-spec projections to namespaced `{name}/prX-Y/VERSION` format
    
    * Updated tests for namespaced non-root-spec projections
    
    * Update prerelease module argument from --root-spec-version to --version
    
    * Pass in sys.argv to parse_args, add tests
    
    * Refactor workflow to split generation of root spec and packages
    
    * Rename projections script to modules
    
    * Add inject_includes function
    
    * For test inputs, comment what is expected
    
    * Reordered functions
    
    * Updated tests after code updates
    
    * Apply suggestions from code review
    
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
    Signed-off-by: Tommy Gatti <tommy.gatti@outlook.com>
    
    * Add root_spec to includes set, but remove it later when alphabetically sorting the includelist since it always comes first
    
    * Updated output of manifest
    
    * Ran black
    
    * Added getter module for accessing various parts of a spack manifest
    
    * Update `[test_]modules.py` to use new getter module
    
    * Update `[test_]prerelease.py` to use getter module, update deploy-2-start to remove --root-spec arg
    
    * Ran black again
    
    * Raise NotImplementedErrors, add comments for successful matches
    
    * Remove comments of old code
    
    * Apply suggestions from code review
    
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
    Signed-off-by: Tommy Gatti <tommy.gatti@outlook.com>
    
    * Add example to multi-target spec to test function
    
    * Parameterized tests
    
    * Fixes following more test cases
    
    * Converted manifest with no packages to fixture
    
    * Improved from_file classmethod tests
    
    * Parameterized valid and mistakenly invalid tests
    
    * Remove `root-spec` arg and instead get from manifest directly, update tests
    
    * Add new prerelease script flag `--keep-root-spec-intact`, that doesn't remove version information
    
    ---------
    
    Signed-off-by: Tommy Gatti <tommy.gatti@outlook.com>
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
    
    * `vars` to File-Based Special Packages (#305)
    
    * Validate MDR config/packages.json
    
    * Pass info from MDR config/packages.json to module injection script
    
    * Update variable name to `vars.CONFIG_PACKAGES_SCHEMA_VERSION`
    
    * Change cwd relative to build-cd rather than model for scripts
    
    * Add output of packages to be injected/added to provenance DB
    
    * Update prerelease projection to `ROOT_SPEC/prX-Y/{name}/VERSION`
    
    * Update tests for new prerelease injection logic
    
    * Replace all internal refs with v6
    
    * Dump quoted strings for prerelease projections, updated expected output
    
    * Schema Versions as Entrypoint Inputs Over `vars` (#310)
    
    * Change schema vars to required inputs, add optional spack manifest schema path for potentially different schemas
    
    * Add required schema version inputs, and optional spack manifest schema paths to entrypoint workflows
    
    * Get packages for provenance from an earlier step
    
    * Update prerelease injection to the form `ROOT_SPEC/.dependencies/PRX-Y/VERSION-{hash:7}`
    
    * Fixed up an unused variable
    
    * Remove `.` from `dependencies directory
    
    ---------
    
    Signed-off-by: Tommy Gatti <tommy.gatti@outlook.com>
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@anu.edu.au>

---
+++ scripts/spack_manifest/injection/prerelease.py
@@ -0,0 +1,216 @@
+import argparse
+import yaml
+import re
+import sys
+
+from typing import Any
+from copy import deepcopy
+
+from scripts.spack_manifest.getter import (
+    RootSpec,
+    Projections,
+)
+
+
+# PyYaml by default dumps unquoted strings if they look unambiguous, and quoted strings otherwise.
+# PyYaml dumps '{name}/prX-Y' as a quoted str as it has '{' at the front and causes ambiguity
+# But 'ROOT_SPEC/.dependencies/prX-Y/VERSION-{hash:7}' is dumped as an unquoted str as it is unambiguous
+# So we need to wrap projections in a custom class that forces PyYaml to dump them as quoted strings.
+class YamlExplicitQuotedString(str):
+    pass
+
+
+def yaml_explicit_quoted_string_representer(dumper, data):
+    """
+    Custom representer for YAML to ensure that some strings are quoted explicitly.
+    This is necessary for strings that are used as projections in spack manifests.
+    """
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="'")
+
+
+yaml.add_representer(YamlExplicitQuotedString, yaml_explicit_quoted_string_representer)
+
+### Actual methods begin here ###
+
+
+def inject_prerelease_information(
+    manifest_path: str,
+    version: str,
+    keep_root_spec_intact: bool = False,
+    spack_packages_path: str | None = None,
+) -> str:
+    # In comparison to the projections script, this returns a string rather than a dict because we need to
+    # add spack-specific, non-standard 'repo::' sections, which the yaml dumper does not support.
+    with open(manifest_path, "r") as manifest_file:
+        manifest: dict[str, Any] = yaml.safe_load(manifest_file)
+
+    root_spec_from_manifest = RootSpec(manifest)
+    root_spec_name = root_spec_from_manifest.get_name()
+
+    updated_manifest: dict[str, Any] = deepcopy(manifest)
+
+    # Remove @git.VERSION information from the root spec, since it will be a tag that does not yet exist for prereleases
+    # This does not include versions of the form @VERSION, which are the hallmark of software deployment repositories,
+    # or builds that explicitly ask to keep_root_spec_intact.
+    if not keep_root_spec_intact:
+        updated_manifest = remove_potential_root_spec_git_version(manifest)
+
+    # We want the root spec projection to be of the form {name}/prX-Y
+    updated_manifest = update_root_spec_projection_version(
+        updated_manifest, root_spec_name, version
+    )
+
+    # We want all other projections to be of the form {name}/prX-Y/VERSION
+    updated_manifest = add_namespace_to_other_projection_versions(
+        updated_manifest, root_spec_name, version
+    )
+
+    # Dump the current dict, and add the non-standard 'repo::' section
+    manifest_str: str = yaml.dump(
+        updated_manifest, default_flow_style=False, sort_keys=False
+    )
+
+    if spack_packages_path:
+        # Add the 'repo::' section for prerelease spack packages if provided
+        manifest_str = add_prerelease_repos_section(manifest_str, spack_packages_path)
+
+    return manifest_str
+
+
+def add_namespace_to_other_projection_versions(
+    manifest: dict[str, Any], root_spec_name: str, version: str
+) -> dict[str, Any]:
+    # We don't want to modify the linked manifest dict, so we create a mutable copy.
+    mutable_manifest: dict[str, Any] = deepcopy(manifest)
+    projections_from_manifest = Projections(mutable_manifest)
+    projections = projections_from_manifest.get()
+
+    # We only want to modify projections that are not the root spec, since that already has its version set
+    if root_spec_name in projections:
+        projections.pop(root_spec_name)
+
+    for projection_name, projection_value in projections.items():
+        # Non-root-spec projections will be of the form ROOT_SPEC_NAME/prX-Y/{name}/VERSION, where VERSION is previously defined.
+        # For example, access-om2/pr12-13/mom5/main-{hash:7}
+        new_projection_value = re.sub(
+            r"{name}/(.+)", rf"{root_spec_name}/dependencies/{version}/{{name}}/\1", projection_value
+        )
+
+        print(
+            f"Updating projection '{projection_name}' from '{projection_value}' to '{new_projection_value}'"
+        )
+
+        # Ensures that the new projection is a quoted string when dumped so spack does projected modules correctly, see top of file.
+        manifest["spack"]["modules"]["default"]["tcl"]["projections"][projection_name] = YamlExplicitQuotedString(new_projection_value)
+
+    return manifest
+
+
+def remove_potential_root_spec_git_version(manifest: dict[str, Any]) -> dict[str, Any]:
+    """
+    Remove the version information from the root spec in the manifest.
+    This is necessary for prerelease deployments where the version may not yet exist.
+    """
+    root_spec_from_manifest = RootSpec(manifest)
+    name = root_spec_from_manifest.get_name()
+    constraints = root_spec_from_manifest.get_non_version_constraints()
+
+    if root_spec_from_manifest.has_git_ref():
+        # Remove the @git version and then add later contraints back
+        manifest["spack"]["specs"][0] = f"{name} {constraints}".strip()
+    else:
+        print(
+            f"The root spec '{name}' does not have a git ref, so no changes are made."
+        )
+
+    return manifest
+
+
+def update_root_spec_projection_version(
+    manifest: dict[str, Any], root_spec_name: str, root_spec_version: str
+) -> dict[str, Any]:
+    updated_version: str = f"{{name}}/{root_spec_version}"
+
+    manifest.setdefault("spack", {}).setdefault("modules", {}).setdefault("default", {}).setdefault("tcl", {}).setdefault("projections", {})
+
+    manifest["spack"]["modules"]["default"]["tcl"]["projections"][root_spec_name] = updated_version
+
+    return manifest
+
+
+def add_prerelease_repos_section(manifest_str: str, spack_packages_path: str) -> str:
+    manifest_str += (
+        f"  repos::\n"
+        f"  - {spack_packages_path}\n"
+        f"  - $spack/var/spack/repos/builtin\n"
+    )
+
+    return manifest_str
+
+
+def parse_args(args: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Script for injecting prerelease information into spack manifest files."
+    )
+
+    ## Args dealing with inputs
+    parser.add_argument(
+        "--manifest",
+        type=str,
+        required=True,
+        help="Path to the spack manifest file to be injected with prerelease information",
+    )
+
+    parser.add_argument(
+        "--version",
+        type=str,
+        required=True,
+        help="Version to be used for projections in the manifest",
+    )
+
+    # This option is for the special case where the root spec defined at the repository level (a bundle with
+    # a version that doesn't yet exist) is not the same as the root spec defined in the manifest (which could
+    # be a regular package with a meaningful version). This is not recommended, but can be useful for special builds.
+    parser.add_argument(
+        "--keep-root-spec-intact",
+        action="store_true",
+        help="If set, the root spec will not be modified to remove git version information.",
+    )
+
+    parser.add_argument(
+        "--spack-packages-path",
+        type=str,
+        required=False,
+        help="Local path to a spack-packages repository that is added to the manifests repos section",
+    )
+
+    # Args dealing with outputs
+    parser.add_argument(
+        "--output",
+        type=str,
+        required=False,
+        help="Path to the output file where the modified manifest will be saved",
+    )
+
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+
+    injected_manifest: str = inject_prerelease_information(
+        args.manifest,
+        args.version,
+        args.keep_root_spec_intact,
+        args.spack_packages_path,
+    )
+
+    print(injected_manifest)
+
+    if args.output:
+        with open(args.output, "w") as output_file:
+            output_file.write(injected_manifest)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/scripts/spack_manifest/test_getter.py
@@ -0,0 +1,450 @@
+import pytest
+import yaml
+
+from scripts.spack_manifest.getter import (
+    RootSpec,
+    Packages,
+    Includes,
+    Projections,
+    NoSectionError,
+    NoSectionComponentError,
+)
+
+### Global Fixtures ###
+
+
+@pytest.fixture
+def manifest_from_file():
+    return {
+        "spack": {
+            "specs": ["access-om2@git.1.0.0"],
+            "packages": {
+                "package1": {"require": ["@git.1.0.0"]},
+                "package2": {"require": ["@git.2.0.0"]},
+            },
+            "modules": {
+                "default": {
+                    "tcl": {
+                        "include": ["root-spec", "package2", "package3"],
+                        "projections": {
+                            "package1": "package1/1.0.0",
+                            "package2": "package2/2.0.0",
+                        },
+                    }
+                }
+            },
+        }
+    }
+
+
+class TestRootSpecGetter:
+    def test___init___valid_multi_target_spec(self):
+        # This is a pared-down example due to json being annoying to represent without many lines of code.
+        # In practice, the multi-target spec looks something like:
+        # spack:
+        #   definitions:
+        #     - ROOT_PACKAGE: [access-om2@git.2025.05]
+        #     - ROOT_SPEC:
+        #         - matrix:
+        #             - [$ROOT_PACKAGE]
+        #             - ['%intel@2021.2.0', '%intel@2021.10.0']
+        #   spec:
+        #     - $ROOT_SPEC
+        manifest = {
+            "spack": {"definitions": [{"ROOT_PACKAGE": ["access-om2@git.1.0.0"]}]}
+        }
+
+        root_spec_getter = RootSpec(manifest)
+
+        assert (
+            root_spec_getter.root_spec == "access-om2@git.1.0.0"
+        ), "Root spec should be correctly initialized from multi-target manifest."
+
+    def test___init___valid_single_target_spec(self):
+        manifest = {"spack": {"specs": ["access-om2@git.1.0.0"]}}
+
+        root_spec_getter = RootSpec(manifest)
+
+        assert (
+            root_spec_getter.root_spec == "access-om2@git.1.0.0"
+        ), "Root spec should be correctly initialized from single-target manifest."
+
+    def test___init___invalid_no_root_spec(self):
+        manifest = {"spack": {"specs": []}}
+
+        with pytest.raises(NoSectionError):
+            RootSpec(manifest)
+
+    def test___init___invalid_no_manifest(self):
+        with pytest.raises(NoSectionError):
+            RootSpec({})
+
+    def test_from_file__valid(self, manifest_from_file, tmp_path):
+        manifest_path = tmp_path / "manifest.yaml"
+        with open(manifest_path, "w") as file:
+            yaml.dump(manifest_from_file, file)
+
+        root_spec_getter = RootSpec.from_file(tmp_path / "manifest.yaml")
+
+        assert (
+            root_spec_getter.root_spec == "access-om2@git.1.0.0"
+        ), "Root spec should be correctly retrieved from file."
+
+    def test_from_file__invalid(self):
+        manifest_path = (
+            "tests/scripts/spack_manifest/injection/inputs/invalid_manifest.yaml"
+        )
+
+        with pytest.raises(OSError):
+            RootSpec.from_file(manifest_path)
+
+    @pytest.mark.parametrize(
+        "spec, expected_name",
+        [
+            ("access-om2@git.1.0.0", "access-om2"),
+            ("access-esm1p6@git.2025.05+debug", "access-esm1p6"),
+            ("access-om2@1.0.0 %intel@2021.2.0 +debug", "access-om2"),
+        ],
+    )
+    def test_get_name__valid(self, spec, expected_name):
+        manifest = {"spack": {"specs": [spec]}}
+
+        assert (
+            RootSpec(manifest).get_name() == expected_name
+        ), "Root spec name should be correctly extracted."
+
+    @pytest.mark.parametrize(
+        "spec",
+        [
+            "@git.1.0.0",
+            "%compiler@2.0.0",
+            "+debug",
+        ],
+    )
+    def test_get_name__invalid(self, spec):
+        manifest = {"spack": {"specs": [spec]}}
+
+        with pytest.raises(NoSectionComponentError):
+            RootSpec(manifest).get_name()
+
+    @pytest.mark.parametrize(
+        "spec, expected_ref",
+        [
+            ("access-om2@git.1.0.0", "1.0.0"),
+            ("access-om2@git.1.0.0 %compiler@2.0.0", "1.0.0"),
+            ("access-om2@git.1.0.0 %compiler@2.0.0 +debug", "1.0.0"),
+            ("access-om2@git.1.0.0=develop %compiler@2.0.0 +debug", "1.0.0"),
+            ("access-om2@git.1.0.0=latest %compiler@2.0.0 +debug", "1.0.0"),
+            ("access-esm1p5@git.develop=access-esm1p5 +debug", "develop"),
+            ("access-esm1p5@git.2025.05+debug", "2025.05"),
+        ],
+    )
+    def test_get_ref__valid(self, spec, expected_ref):
+        manifest = {"spack": {"specs": [spec]}}
+
+        assert (
+            RootSpec(manifest).get_ref() == expected_ref
+        ), "Root spec ref should be correctly extracted."
+
+    @pytest.mark.parametrize(
+        "spec",
+        [
+            "access-om2",
+            "access-om2+debug",
+            "access-om2%intel@2025.05",
+        ],
+    )
+    def test_get_ref__invalid(self, spec):
+        manifest = {"spack": {"specs": [spec]}}
+
+        with pytest.raises(NoSectionComponentError):
+            RootSpec(manifest).get_ref()
+
+    @pytest.mark.parametrize(
+        "root_spec",
+        [
+            "",
+            "%compiler@2.0.0",
+            " %compiler@2.0.0 +debug",
+            "+debug",
+            "~debug",
+            " +debug",
+            " ~debug",
+        ],
+    )
+    def test_get_non_version_constraints__valid(self, root_spec):
+        manifest = {"spack": {"specs": [f"access-om2@git.1.0.0{root_spec}"]}}
+
+        assert (
+            RootSpec(manifest).get_non_version_constraints() == root_spec.strip()
+        ), "Non-version constraints should be correctly extracted."
+
+    @pytest.mark.parametrize(
+        "root_spec, expected",
+        [
+            ("access-om2@git.1.0.0", True),
+            ("access-om2@1.0.0", False),
+        ],
+    )
+    def test_has_git_ref__valid(self, root_spec, expected):
+        manifest = {"spack": {"specs": [root_spec]}}
+
+        assert RootSpec(manifest).has_git_ref() == expected
+
+
+class TestPackagesGetter:
+    ### Fixtures ###
+
+    @pytest.fixture
+    def manifest_with_no_packages(self):
+        return {"spack": {"packages": {}}}
+
+    ### Tests ###
+
+    @pytest.mark.parametrize(
+        "packages",
+        [
+            {
+                "package1": {"require": ["@git.1.0.0"]},
+                "package2": {"require": ["@git.2.0.0"]},
+            },
+            {},
+        ],
+    )
+    def test___init___valid(self, packages):
+        manifest = {"spack": {"packages": packages}}
+
+        packages_getter = Packages(manifest)
+
+        assert (
+            packages_getter.packages == packages
+        ), "Packages should be correctly initialized from manifest."
+
+    def test___init___valid_no_packages(self):
+        manifest = {"spack": {"packages": {}}}
+
+        packages_getter = Packages(manifest)
+
+        assert (
+            packages_getter.packages == {}
+        ), "Packages should be initialized as an empty dictionary if no packages are defined."
+
+    def test___init___invalid_no_packages_section(self):
+        manifest = {"spack": {}}
+
+        with pytest.raises(NoSectionError):
+            Packages(manifest)
+
+    def test_from_file__valid(self, tmp_path, manifest_from_file):
+        manifest_path = tmp_path / "manifest.yaml"
+        with open(manifest_path, "w") as file:
+            yaml.dump(manifest_from_file, file)
+
+        assert Packages.from_file(manifest_path).manifest == manifest_from_file
+
+    def test_from_file__invalid(self):
+        manifest_path = (
+            "tests/scripts/spack_manifest/injection/inputs/invalid_manifest.yaml"
+        )
+
+        with pytest.raises(OSError):
+            Packages.from_file(manifest_path)
+
+    def test_get_package_requirements__valid(self):
+        manifest = {
+            "spack": {
+                "packages": {
+                    "package1": {"require": ["@git.1.0.0"]},
+                }
+            }
+        }
+
+        packages_getter = Packages(manifest)
+        requirements = packages_getter.get_package_requirements("package1")
+
+        assert requirements == {
+            "require": ["@git.1.0.0"]
+        }, "Package requirements should be correctly retrieved."
+
+    def test_get_package_requirements__invalid_no_package(
+        self, manifest_with_no_packages
+    ):
+        packages_getter = Packages(manifest_with_no_packages)
+        with pytest.raises(NoSectionComponentError):
+            packages_getter.get_package_requirements("nonexistent_package")
+
+    @pytest.mark.parametrize("version_req", ["@git.1.0.9", "@2.0.0"])
+    def test_get_package_full_version_requirement__valid(self, version_req):
+        manifest = {
+            "spack": {
+                "packages": {
+                    "package1": {"require": [version_req, "+debug"]},
+                }
+            }
+        }
+
+        packages_getter = Packages(manifest)
+        full_version = packages_getter.get_package_full_version_requirement("package1")
+
+        assert (
+            full_version == version_req
+        ), "Full version requirement should be correctly retrieved."
+
+    def test_get_package_full_version_requirement__invalid_no_package(
+        self, manifest_with_no_packages
+    ):
+
+        packages_getter = Packages(manifest_with_no_packages)
+        with pytest.raises(NoSectionError):
+            packages_getter.get_package_full_version_requirement("nonexistent_package")
+
+    @pytest.mark.parametrize(
+        "version_reqs",
+        [
+            ["@git.1.0.0", "+debug", "%compiler@2.0.0"],
+            ["@git.2.0.0"],
+        ],
+    )
+    def test_get_package_requirements__valid(self, version_reqs):
+        manifest = {"spack": {"packages": {"package1": {"require": version_reqs}}}}
+
+        packages_getter = Packages(manifest)
+        requirements = packages_getter.get_package_requirements("package1")
+
+        assert (
+            requirements == version_reqs
+        ), "Package requirements should be correctly retrieved."
+
+    def test_get_package_requirements__invalid_no_requirements(self):
+        manifest = {"spack": {"packages": {"package1": {}}}}
+
+        packages_getter = Packages(manifest)
+
+        assert (
+            packages_getter.get_package_requirements("package1") == []
+        ), "Package with no requirements should return an empty list."
+
+    @pytest.mark.parametrize(
+        "ref_requirement, expected_ref",
+        [
+            ("@git.1.0.0", "1.0.0"),
+            ("@2.0.0", "2.0.0"),
+            ("@git.2025.05+debug", "2025.05"),
+        ],
+    )
+    def test_get_package_ref_requirement__valid(self, ref_requirement, expected_ref):
+        manifest = {
+            "spack": {
+                "packages": {
+                    "package1": {"require": [ref_requirement]},
+                }
+            }
+        }
+
+        packages_getter = Packages(manifest)
+        ref = packages_getter.get_package_ref_requirement("package1")
+
+        assert (
+            ref == expected_ref
+        ), "Package ref requirement should be correctly retrieved."
+
+    def test_get_package_ref_requirement__invalid_no_package(
+        self, manifest_with_no_packages
+    ):
+        packages_getter = Packages(manifest_with_no_packages)
+        with pytest.raises(NoSectionComponentError):
+            packages_getter.get_package_ref_requirement("nonexistent_package")
+
+
+class TestProjectionsGetter:
+    def test_from_file__invalid(self):
+        manifest_path = (
+            "tests/scripts/spack_manifest/injection/inputs/invalid_manifest.yaml"
+        )
+
+        with pytest.raises(OSError):
+            Projections.from_file(manifest_path)
+
+    def test__get_defined_projections__valid(self):
+        manifest = {
+            "spack": {
+                "modules": {
+                    "default": {
+                        "tcl": {
+                            "projections": {
+                                "package1": "package1/1.0.0",
+                                "package2": "package2/2.0.0",
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        expected = {"package1": "package1/1.0.0", "package2": "package2/2.0.0"}
+
+        result = Projections(manifest).get()
+
+        assert (
+            result == expected
+        ), "A valid manifest should return all defined projections."
+
+    def test__get_defined_projections__no_projections(self):
+        manifest = {"spack": {"modules": {"default": {"tcl": {"projections": {}}}}}}
+
+        expected = dict()
+
+        result = Projections(manifest).get()
+
+        assert (
+            result == expected
+        ), "Manifest without projections should return an empty set."
+
+    def test__get_defined_projections__no_modules_section(self):
+        manifest = {"spack": {}}
+
+        expected = dict()
+
+        result = Projections(manifest).get()
+
+        assert (
+            result == expected
+        ), "Manifest without a modules section should return an empty set."
+
+
+class TestIncludesGetter:
+    def test_from_file__invalid(self):
+        manifest_path = (
+            "tests/scripts/spack_manifest/injection/inputs/invalid_manifest.yaml"
+        )
+
+        with pytest.raises(OSError):
+            Includes.from_file(manifest_path)
+
+    @pytest.mark.parametrize(
+        "includes",
+        [
+            ["root-spec", "package2", "package3"],
+            ["root-spec"],
+            [],
+        ],
+    )
+    def test__get_defined_includes__valid(self, includes):
+        manifest = {"spack": {"modules": {"default": {"tcl": {"include": includes}}}}}
+
+        result = Includes(manifest).get()
+
+        assert (
+            result == includes
+        ), "A valid manifest should return all defined includes."
+
+    def test__get_defined_includes__no_modules_section(self):
+        manifest = {"spack": {}}
+
+        expected = []
+
+        result = Includes(manifest).get()
+
+        assert (
+            result == expected
+        ), "Manifest without a modules section should return an empty set."
